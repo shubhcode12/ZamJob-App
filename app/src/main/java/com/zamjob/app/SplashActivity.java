77c6376ac0f25cc114f22e222b7a866b6ad1ff24
shubhcode12 <shubhmahajan1234@gmail.com>
2022-11-25 18:30:18 +0530

    Add : set animation to splash shapes

---
+++ app/src/main/java/com/zamjob/app/SplashActivity.java
@@ -0,0 +1,38 @@
+package com.zamjob.app;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+
+import com.zamjob.app.databinding.ActivitySplashBinding;
+
+public class SplashActivity extends AppCompatActivity {
+    ActivitySplashBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivitySplashBinding.inflate(getLayoutInflater());
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(binding.getRoot());
+
+        Animation animSlideInRight = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.anim_slide_in_right);
+        Animation animSlideInLeft = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.anim_slide_in_left);
+        Animation animSlideInTopt = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.anim_slide_in_top);
+
+        binding.shape1.setAnimation(animSlideInTopt);
+        binding.shape2.setAnimation(animSlideInLeft);
+        binding.shape3.setAnimation(animSlideInRight);
+        binding.shape4.setAnimation(animSlideInLeft);
+
+
+        binding.btnSkip.setOnClickListener(view -> {
+            startActivity(new Intent(this, MainActivity.class));
+            finish();
+        });
+
+    }
+}
