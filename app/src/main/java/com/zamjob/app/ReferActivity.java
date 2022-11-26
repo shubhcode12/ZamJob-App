67c7c559be4f2a2f3411ba2d8341593c6f030044
shubhcode12 <shubhmahajan1234@gmail.com>
2022-11-27 01:14:52 +0530

    Add : Refer Layout

---
+++ app/src/main/java/com/zamjob/app/ReferActivity.java
@@ -0,0 +1,20 @@
+package com.zamjob.app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.zamjob.app.databinding.ActivityReferBinding;
+
+public class ReferActivity extends AppCompatActivity {
+    ActivityReferBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityReferBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+
+    }
+}
