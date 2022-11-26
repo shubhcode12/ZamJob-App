2607c7b18580c87e0cc26d7544202dcf0f818a64
shubhcode12 <shubhmahajan1234@gmail.com>
2022-11-27 02:26:56 +0530

    Add : setup setting vbiewbinding

--- app/src/main/java/com/zamjob/app/SettingActivity.java
+++ app/src/main/java/com/zamjob/app/SettingActivity.java
@@ -4,11 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.zamjob.app.databinding.ActivitySettingBinding;
+
 public class SettingActivity extends AppCompatActivity {
+    ActivitySettingBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_setting);
+        binding = ActivitySettingBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
     }
 }
