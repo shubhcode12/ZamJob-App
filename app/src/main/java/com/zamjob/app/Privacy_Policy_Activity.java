c667c3b9323500d7566719cedc1d27e6da56443d
shubhcode12 <shubhmahajan1234@gmail.com>
2022-11-27 02:06:50 +0530

    Add : privacy policy layout

---
+++ app/src/main/java/com/zamjob/app/Privacy_Policy_Activity.java
@@ -0,0 +1,18 @@
+package com.zamjob.app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.zamjob.app.databinding.ActivityPrivacyPolicyBinding;
+
+public class Privacy_Policy_Activity extends AppCompatActivity {
+    ActivityPrivacyPolicyBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityPrivacyPolicyBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+    }
+}
