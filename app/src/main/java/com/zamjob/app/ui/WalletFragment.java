e3e71f98437d97f8684284e2948a923af1fcec00
shubhcode12 <shubhmahajan1234@gmail.com>
2022-11-26 21:50:07 +0530

    Design : Wallet screen

--- app/src/main/java/com/zamjob/app/ui/WalletFragment.java
+++ app/src/main/java/com/zamjob/app/ui/WalletFragment.java
@@ -6,7 +6,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.Toast;
+import com.google.android.material.tabs.TabLayout;
 import com.zamjob.app.databinding.FragmentWalletBinding;
 
 public class WalletFragment extends Fragment {
@@ -17,8 +18,46 @@ public class WalletFragment extends Fragment {
         binding = FragmentWalletBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
+        binding.tab.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
 
+                if(tab.getPosition() == 0){
+                    Toast.makeText(getActivity(), "Transactions", Toast.LENGTH_SHORT).show();
+                    binding.LinTrans.setVisibility(View.VISIBLE);
+                    binding.LinReedem.setVisibility(View.GONE);
+
+                    /*uncomment this later*/
+//                    binding.rvTransactions.setVisibility(View.VISIBLE);
+//                    binding.rvReedemed.setVisibility(View.GONE);
+                }else if(tab.getPosition() == 1){
+                    Toast.makeText(getActivity(), "Reeedemed", Toast.LENGTH_SHORT).show();
+                    binding.LinReedem.setVisibility(View.VISIBLE);
+                    binding.LinTrans.setVisibility(View.GONE);
+
+                    /*uncomment this later*/
+//                    binding.rvReedemed.setVisibility(View.VISIBLE);
+//                    binding.rvTransactions.setVisibility(View.GONE);
+                }
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
         return root;
+    }
+
+    @Override
+    public void onStart() {
+        binding.tab.getTabAt(1).select();
+        super.onStart();
     }
 
     @Override
