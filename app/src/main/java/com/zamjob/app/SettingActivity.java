55e6ad4e28cac2767489400cbc76d3fd962f71cc
shubhcode12 <shubhmahajan1234@gmail.com>
2022-11-27 16:21:14 +0530

    Code cleanup

--- app/src/main/java/com/zamjob/app/SettingActivity.java
+++ app/src/main/java/com/zamjob/app/SettingActivity.java
@@ -1,9 +1,9 @@
 package com.zamjob.app;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.zamjob.app.databinding.ActivitySettingBinding;
 
