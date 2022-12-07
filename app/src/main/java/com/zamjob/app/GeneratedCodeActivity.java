3d3ce27a699c459ab14e4e65097b0a644c847eae
shubhcode12 <shubhmahajan1234@gmail.com>
2022-12-07 13:42:25 +0530

    Add : setup viewbinding activity

--- app/src/main/java/com/zamjob/app/GeneratedCodeActivity.java
+++ app/src/main/java/com/zamjob/app/GeneratedCodeActivity.java
@@ -4,11 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.zamjob.app.databinding.ActivityGeneratedCodeBinding;
+
 public class GeneratedCodeActivity extends AppCompatActivity {
+    ActivityGeneratedCodeBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_generated_code);
+        binding = ActivityGeneratedCodeBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
     }
 }
